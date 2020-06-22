dfeb88ee5cb94ab0f5e1002fc80f8f29067e7a82
rahulshahaha <getrshah@gmail.com>
2020-06-22 08:54:31 -0400

    setup

---
+++ doodad/src/App.js
@@ -0,0 +1,292 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+
+
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-167301971-1"></script>
+
+
+
+
+
+	<title>Doodad Capital</title>
+	<meta name="description" content="Equity research group"/>
+	<meta name="author" content="Doodad Capital"/>
+
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+
+	
+   <link rel="stylesheet" href="css/base.css"/>
+	<link rel="stylesheet" href="css/layout.css"/>
+
+
+
+	<link rel="shortcut icon" href="tabLogo2.jpg" />
+
+
+
+
+
+
+   <header className="mobile">
+
+      <div className="row">
+
+         <div className="col full">
+
+            <div className="logo">
+               <a href="http://doodadcapital.com"><img alt="" src="images/straightLogo.png"/></a>
+            </div>
+
+            <nav id="nav-wrap">
+
+               <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+	            <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+
+               <ul id="nav" className="nav">
+	               <li><a href="#intro">Home</a></li>
+	               <li><a href="#journal">Articles</a></li>
+               </ul>
+
+            </nav>
+
+         </div>
+
+      </div>
+
+   </header> 
+
+
+
+   <section id="intro">
+
+
+	   <div id="intro-slider" className="flexslider">
+
+		   <ul className="slides">
+
+
+			   <li>
+				   <div className="row">
+					   <div className="col full">
+						   <div className="slider-text">
+							   <h2>Hello, we are <span className="highlightText">Doodad Capital.</span> We specialize in equity research for <span className="highlightText">traditionally uncovered companies.</span></h2>
+						   </div>
+					   </div>
+				   </div>
+			   </li>
+
+
+
+		   </ul>
+	   </div>
+	   
+
+   </section> 
+
+
+
+
+   <section id="journal">
+
+      <div className="row section-head">
+         <div className="col full">
+            <h2>Articles</h2>
+            <p className="desc">Our latest articles.</p>
+            <p><a href="blog.html" className="more-link">See all articles <i className="icon-angle-right"></i></a></p>
+         </div>
+         
+      </div>
+      
+
+
+      <div className="blog-entries">
+
+        
+         <article className="entry">
+
+            <div className="row entry-header">
+
+               <div className="author-image">
+                  <img src="images/user-03.png" alt="Sakura Haruno" />
+               </div>
+
+               <div className="col g-9 offset-1 entry-title">
+                  <h3><a href="articles/mesa.html">Mesa Air Group (MESA)</a></h3>
+               </div>
+
+               <div className="col g-2">
+                  <p className="post-meta">
+                     <time pubdate="" className="post-date" dateTime="2020-06-21">June 21, 2020</time>
+                     <span className="dauthor">By Doodad Capital</span>
+                  </p>
+               </div>
+
+            </div>
+
+            <div className="row">
+
+               <div className="col g-9 offset-1 post-content">
+                  <p>Mesa Air Group is a profitable business that has been severely impacted by the Covid-19. The company has some financial strength and a favorable cost structure but comes with some big risks and a lot of debt. Mesa could be a good buy for less risk averse investors.
+                  <a href="articles/mesa.html" className="more-link">Read More<i className="icon-angle-right"></i></a></p>
+               </div>
+               <hr></hr>
+
+
+            </div>
+
+         </article> 
+
+ 
+         <article className="entry">
+
+            <div className="row entry-header">
+
+               <div className="author-image">
+                  <img src="images/user-03.png" alt="Sakura Haruno" />
+               </div>
+
+               <div className="col g-9 offset-1 entry-title">
+                  <h3><a href="articles/vtiq.html">What is VectoIQ (VTIQ) and why has it been increasing so much?</a></h3>
+               </div>
+
+               <div className="col g-2">
+                  <p className="post-meta">
+                     <time pubdate="" className="post-date" dateTime="2020-05-28">May 28, 2020</time>
+                     <span className="dauthor">By Doodad Capital</span>
+                  </p>
+               </div>
+
+            </div>
+
+            <div className="row">
+
+               <div className="col g-9 offset-1 post-content">
+                  <p>VTIQ will complete the process of acquiring Nikola Motors by the end of June, and powered by the overall market rising, fear of missing out, and hype around this next “Tesla”, the new entity will skyrocket before coming down to reality.
+                  <a href="articles/vtiq.html" className="more-link">Read More<i className="icon-angle-right"></i></a></p>
+               </div>
+
+            </div>
+
+         </article> 
+
+    
+         <article className="entry">
+
+            <div className="row entry-header">
+
+               <div className="author-image">
+                  <img src="images/user-03.png" alt="Sakura Haruno" />
+               </div>
+
+               <div className="col g-9 offset-1 entry-title">
+                  <h3><a href="articles/spac.html">What is a special purpose aquisition company (SPAC)?</a></h3>
+               </div>
+
+               <div className="col g-2">
+                  <p className="post-meta">
+                     <time pubdate="" className="post-date" dateTime="2020-05-27">May 27, 2020</time>
+                     <span className="dauthor">By Doodad Capital</span>
+                  </p>
+               </div>
+
+            </div>
+
+            <div className="row">
+
+               <div className="col g-9 offset-1 post-content">
+                  <p>Special purpose acquisition companies explained
+                  <a href="articles/spac.html" className="more-link">Read More<i className="icon-angle-right"></i></a></p>
+               </div>
+
+            </div>
+
+         </article>
+
+
+         <article className="entry">
+
+            <div className="row entry-header">
+
+               <div className="author-image">
+                  <img src="images/user-03.png" alt="Sakura Haruno" />
+               </div>
+
+               <div className="col g-9 offset-1 entry-title">
+                  <h3><a href="articles/agys.html">Agilysys (AGYS)</a></h3>
+               </div>
+
+               <div className="col g-2">
+                  <p className="post-meta">
+                     <time pubdate="" className="post-date" dateTime="2020-05-20">May 20, 2020</time>
+                     <span className="dauthor">By Doodad Capital</span>
+                  </p>
+               </div>
+
+            </div>
+
+            <div className="row">
+
+               <div className="col g-9 offset-1 post-content">
+                  <p>We think that Agilysys (AGYS) is a good buy due to its coronavirus resilient business model and strong financial footing.
+                  <a href="articles/agys.html" className="more-link">Read More<i className="icon-angle-right"></i></a></p>
+               </div>
+               <hr></hr>
+
+
+            </div>
+
+         </article> 
+
+
+
+      </div>
+
+   </section> 
+
+
+
+   
+
+
+   <footer>
+
+      <div className="row">
+
+         <div className="col g-7">
+            <ul className="copyright">
+               <li>&copy; 2014 Kreative</li>
+               <li>Design by <a href="http://www.styleshout.com/" title="Styleshout">Styleshout</a></li>               
+            </ul>
+         </div>
+
+
+
+      </div>
+
+   </footer> 
+
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+   <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+
+   <script src="js/scrollspy.js"></script>
+   <script src="js/jquery.flexslider.js"></script>
+   <script src="js/jquery.reveal.js"></script>
+   <script src="https://maps.google.com/maps/api/js?sensor=true" type="text/javascript"></script>
+   <script src="js/gmaps.js"></script>
+   <script src="js/init.js"></script>
+   <script src="js/smoothscrolling.js"></script>
+
+
+    </div>
+  );
+}
+
+export default App;
