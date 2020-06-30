fd244b77c9ca2f8cc2f63a3a2ac33c354bf5f4da
rahulshahaha <getrshah@gmail.com>
2020-06-30 12:21:07 -0400

    Home page

--- doodad/src/components/Articles.js
+++ doodad/src/components/Articles.js
@@ -18,16 +18,33 @@ class Articles extends React.Component {
 
         //const sanitizer = dompurify.sanitize;
 
+        articles.sort(function(a,b){
+            var returnValue = 0;
+            if(a.createdDate === undefined || b.createdDate === undefined){
+                return returnValue;
+            }
+            //return a.attributes.OBJECTID - b.attributes.OBJECTID;
+            if(a.createdDate.toDate() === b.createdDate.toDate())
+                returnValue = 0;
+            if(a.createdDate.toDate() < b.createdDate.toDate())
+                returnValue = 1;
+            if(a.createdDate.toDate() > b.createdDate.toDate())
+                returnValue = -1;
+
+           return returnValue
+        });
+
         var articleList = articles.map(function(s){
+
             var link = "/articles/" + s.key
             return (
             <div key={s.key}>
-            <h1><Link to={{
+            <Link className="articleTitle" to={{
                 pathname: link,
                 state: {
                     key: s.key
                 }
-            }}>{s.title}</Link></h1>
+            }}>{s.title}</Link>
             <p>{s.lead}</p>
             </div>
             );
--- doodad/src/components/Home.js
+++ doodad/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "../css/Home.css"
+import Articles from './Articles';
 
 
 
@@ -10,7 +11,15 @@ class Home extends React.Component {
 render(){
     return (
       <div className="Home">
-        <p>Home</p>
+        <section className="coverPhoto">
+        <div className="titleText">
+          <h2>Hello, we are <span className="highlightText">Doodad Capital.</span> We specialize in equity research for <span className="highlightText">traditionally uncovered companies.</span></h2>
+        </div>
+        </section>
+        <div className="articleListHeader">
+          <h2>Articles</h2>
+        </div>
+        <Articles></Articles>
       </div>
     );
   }
