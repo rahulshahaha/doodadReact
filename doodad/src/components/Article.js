e10ffb386536df00168ad6bd826adc82d575230d
rahulshahaha <getrshah@gmail.com>
2020-07-01 16:46:14 -0400

    fixed dates

--- doodad/src/components/Article.js
+++ doodad/src/components/Article.js
@@ -15,8 +15,8 @@ render(){
     title = this.props.article === undefined ? "Cant find article" : this.props.article.title;
     var content = this.props.article === undefined ? "" : this.props.article.content;
     var date = this.props.article === undefined ? "" : this.props.article.createdDate.toDate()
+    var ISODate = this.props.article === undefined ? "" : date.toISOString();
     date = this.props.article === undefined ? "" : ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + date.getFullYear();
-    var ISODate = this.props.article === undefined ? "" : new Date(date);
     var lead = this.props.article === undefined ? "" : this.props.article.lead;
 
     return (
